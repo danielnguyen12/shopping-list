89dc931edbb2257f70e943c8bab9ff2363025248
Daniel Nguyen <daniel.nguye@gmail.com>
2023-02-18 10:34:06 -0500

    prevent duplicate items

--- script.js
+++ script.js
@@ -41,6 +41,10 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove('edit-mode');
     itemToEdit.remove();
     isEditMode = false;
+  } else if (checkIfItemExists(newItem)) {
+    alert('That item already exists.');
+    checkState();
+    return;
   }
 
   addItemToDOM(newItem);
@@ -75,6 +79,11 @@ function onClickItem(e) {
   } else {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  return itemsFromStorage.includes(item);
 }
 
 function setItemToEdit(item) {
